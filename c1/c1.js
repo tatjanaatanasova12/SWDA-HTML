47277175499585bebdcb655f973a36fd3c02d17f
tatjanaatanasova12 <43668940+tatjanaatanasova12@users.noreply.github.com>
2019-02-07 19:50:26 +0100

    .

---
+++ c1/c1.js
@@ -0,0 +1,49 @@
+console.log('Hello world');
+
+var ime = 'Bojan'; // string
+var brojNaKokoshki = 10; // int
+const pi = 3.14; // const float
+var visina = 1.73; // float
+var vrneSneg = false; // bool
+
+var neznam = true;
+var kilazha = 90.1;
+var imeAkademija = 'Semos Akademija';
+var brStudenti = 13;
+
+console.log(vrneSneg);
+
+vrneSneg = true;
+
+console.log(vrneSneg);
+
+
+// pi = 2.8; // ova ne raboti!!! konstantite ne se promenlivi
+// console.log(pi);
+
+var edinechniNavodnici = 'test test';
+var dupliNavodnici = "test test";
+var nakoseniNavodnici = `test test`;
+
+console.log(edinechniNavodnici, dupliNavodnici, nakoseniNavodnici);
+
+var godini = 300;
+var recenicaPero = "Pero ima " + godini + " godini";
+console.log(recenicaPero);
+
+var recenicaJanko = 'Janko ima ' + godini + ' godini';
+console.log(recenicaJanko);
+
+var recenicaStanko = `Stanko ima ${godini} godini`;
+console.log(recenicaStanko);
+
+var a = 21;
+var b = 1396;
+var c = a - b;
+console.log(c);
+
+console.log(a - b);
+console.log(a + b);
+console.log(a * b);
+console.log(b / a);
+console.log(b % a); // ostatok od delenje
