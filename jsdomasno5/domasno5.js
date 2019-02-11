e5c606f76f78e011ccd766608cbee96fdc10a200
tatjanaatanasova12 <43668940+tatjanaatanasova12@users.noreply.github.com>
2019-02-11 22:36:06 +0100

    .

---
+++ jsdomasno5/domasno5.js
@@ -0,0 +1,31 @@
+var ime= document.querySelector(".ime");
+var btn= document.querySelector(".btn");
+
+document.addEventListener('click', function(){
+switch(ime.value){ 
+case 'Pero':
+    console.log("Pero"); 
+    break;
+case 'Janko':
+    console.log("Janko"); 
+    break;
+case 'Stanko':
+    console.log("Stanko"); 
+    break;
+case 'Vanco':
+    console.log("Vanco"); 
+    break;
+case 'Elena':
+    console.log("Elena"); 
+    break;
+case 'Vesna':
+    console.log("Vesna"); 
+    break;
+case 'Jana':
+    console.log("Jana"); 
+    break;
+case 'Irena':
+    console.log("Irena"); 
+    break; 
+    }
+});
